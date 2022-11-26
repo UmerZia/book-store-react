ff8fa91deecc0f81283c779e49dc64012c9bf5ad
UmerZia <omerzia73@gmail.com>
2022-11-26 20:21:26 +0500

    Fix linter error

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -7,8 +7,8 @@ const AddBook = () => (
     <section className={style.Container}>
       <h2 className={style.formTitle}>ADD NEW BOOK</h2>
       <form action="./" className={style.addBookForm}>
-        <input className={`${style.input} ${style.titleInput}`} type="text" placeholder="Book title" required Value />
-        <input className={`${style.input} ${style.titleInput}`} type="text" placeholder="Book Author" required Value />
+        <input className={`${style.input} ${style.titleInput}`} type="text" placeholder="Book title" required value />
+        <input className={`${style.input} ${style.titleInput}`} type="text" placeholder="Book Author" required value />
         <button type="button" className={style.btnAddBook}>ADD BOOK</button>
       </form>
     </section>
